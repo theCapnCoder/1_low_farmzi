e38c7e6b2a0cbd3cb1c40d3810a4c72cba479dd7
PavelM <myagkiypavel@gmail.com>
2024-12-03 13:42:00 +0200

    fix: about markUP

--- src/app/_containers/about.tsx
+++ src/app/_containers/about.tsx
@@ -6,13 +6,13 @@ import { Button } from "../_components/button";
 
 export default function About() {
   return (
-    <section>
+    <section className="py-28">
       <Container>
-        <div className="mb-10">
-          <LineText>
-            <h5 className="text-gray_light">About Company</h5>
+        <div className="mb-12">
+          <LineText gray className="mb-2">
+            <h5 className="text-lg text-gray_dark">About Company</h5>
           </LineText>
-          <h3 className="text-3xl text-green_light">
+          <h3 className="max-w-5xl font-playfair_display text-5xl leading-tight text-green_dark">
             Farmzi have 35 years of experience and know smart way to grow and
             design your beautiful garden.
           </h3>
@@ -21,34 +21,40 @@ export default function About() {
         <div className="grid grid-cols-[1fr_1fr] gap-10">
           <Image src={Woman} alt="woman" />
 
-          <div>
-            <h3 className="text-2xl text-green_light">Who we are?</h3>
-            <h5 className="text-gray_light font-roboto">
+          <div className="max-w-xl pl-24 pt-16">
+            <h3 className="mb-8 font-playfair_display text-3xl text-green_dark">
+              Who we are?
+            </h3>
+            <h5 className="mb-4 leading-relaxed text-gray_dark">
               Lorem Ipsum is simply dummy text of the printing and typeset
               industry. Lorem Ipsum has been the industry`s standard dumm ever
               since the 1500s, when an unknown printer took a galley scrambled
               it make a type specimen book.
             </h5>
-            <h4 className="text-green_light">
+            <h4 className="mb-8 text-2xl text-green_dark">
               “Lorem Ipsum is simply dummy text of the printn industry lorem
               epsum has been the industry and scrambled it make a type specimen
               book”.
             </h4>
 
-            <h3 className="text-2xl text-green_light">Mission and vision</h3>
-            <h5 className="text-gray_light">
+            <h3 className="mb-6 font-playfair_display text-3xl text-green_dark">
+              Mission and vision
+            </h3>
+            <h5 className="mb-4 leading-relaxed text-gray_dark">
               Lorem Ipsum is simply dummy text of the printing and typeset
               industry. Lorem Ipsum has been the industry`s standard dumm ever
               since the 1500s, when an unknown printer took a galley scrambled
               it make a type specimen book.
             </h5>
-            <h5 className="text-gray_light">
+            <h5 className="mb-24 leading-relaxed text-gray_dark">
               Lorem Ipsum is simply dummy text of the printing and typeset
               industry. Lorem Ipsum has been the industry`s standard dumm
               scrambled it make a type specimen book.
             </h5>
 
-            <Button className="border-2 border-green_light">Learn More</Button>
+            <Button className="border-2 w-full max-w-52 border-green_dark leading-relaxed text-green_dark">
+              Learn More
+            </Button>
           </div>
         </div>
       </Container>

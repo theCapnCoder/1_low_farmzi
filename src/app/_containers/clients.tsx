e47910805564a333be15ffe52f08e4dbb5183326
PavelM <myagkiypavel@gmail.com>
2024-12-03 16:40:03 +0200

    fix: Clients markUp

--- src/app/_containers/clients.tsx
+++ src/app/_containers/clients.tsx
@@ -1,39 +1,50 @@
 import { Container } from "../_components/container";
 import { LineText } from "../_components/line-text";
 
+const cards = [
+  {
+    id: 1,
+    text: "“Lorem Ipsum is simply dummy text the printing and typesetting industry. Lorem has been the industry's standard dummy text since the 1500 when an unknown printer took galley type and scram bled it make a type specimen book",
+    subtitle: "Designer of Musion",
+    title: "Rachel Patterson",
+  },
+  {
+    id: 2,
+    text: "“Lorem Ipsum is simply dummy text the printing and typesetting industry. Lorem has been the industry's standard dummy text since the 1500 when an unknown printer took galley type and scram bled it make a type specimen book",
+    subtitle: "CEO of Musion",
+    title: "Jhon Doe",
+  },
+];
+
 export default function Clients() {
-  const cards = [
-    {
-      text: "“Lorem Ipsum is simply dummy text the printing and typesetting industry. Lorem has been the industry's standard dummy text since the 1500 when an unknown printer took galley type and scram bled it make a type specimen book",
-      subtitle: "Designer of Musion",
-      title: "Rachel Patterson",
-    },
-    {
-      text: "“Lorem Ipsum is simply dummy text the printing and typesetting industry. Lorem has been the industry's standard dummy text since the 1500 when an unknown printer took galley type and scram bled it make a type specimen book",
-      subtitle: "CEO of Musion",
-      title: "Jhon Doe",
-    },
-  ];
   return (
-    <section className="bg-green_ultra_light py-20">
+    <section className="bg-green_ultra_light py-28">
       <Container>
-        <div className="mb-10">
-          <LineText className="mb-4">
-            <h4 className="text-gray_light">Happy Clients</h4>
+        <div className="mb-12">
+          <LineText gray className="mb-2">
+            <h4 className="text-lg leading-relaxed text-gray_dark">
+              Happy Clients
+            </h4>
           </LineText>
-          <h3 className="text-3xl text-green_light">
+          <h3 className="font-playfair_display text-5xl leading-tight text-green_dark">
             Farmzi got best feedback from happy clients
           </h3>
         </div>
 
         <div className="grid grid-cols-2 gap-14">
           {cards.map((card) => (
-            <div key={card.text} className="bg-white p-14">
-              <p className="mb-10 text-lg text-green_light">{card.text}</p>
-              <LineText className="mb-4">
-                <h5 className="text-gray_light">{card.subtitle}</h5>
+            <div key={card.id} className="bg-white p-16">
+              <p className="mb-20 font-playfair_display text-xl leading-snug text-green_dark">
+                {card.text}
+              </p>
+              <LineText className="mb-4" gray>
+                <h5 className="leading-relaxed text-gray_dark">
+                  {card.subtitle}
+                </h5>
               </LineText>
-              <h3 className="text-xl text-green_light">{card.title}</h3>
+              <h3 className="font-playfair_display text-2xl leading-relaxed text-green_dark">
+                {card.title}
+              </h3>
             </div>
           ))}
         </div>
